c9296ef4ed24c0f67ffd79d2fb2176c0d212d010
Priceless-P <prisca.priceless11@gmail.com>
2023-02-21 23:47:43 +0100

    redesign

---
+++ src/components/index.js
@@ -0,0 +1,329 @@
+function Home() {
+  return (
+    <div className="tokyo_tm_all_wrap" data-magic-cursor data-color="white">
+      <div className="tokyo_tm_modalbox">
+        <div className="box_inner">
+          <div className="close">
+            <a href="#">
+              <img className="svg" src="img/svg/cancel.svg" alt />
+            </a>
+          </div>
+          <div className="description_wrap" />
+        </div>
+      </div>
+      <a href="risca_Maduka_Resume.pdf" download="Prisca Maduka - Resume.pdf" className="tokyo_tm_modalbox_about">
+
+      </a>
+      <div className="tokyo_tm_mobile_menu">
+        <div className="menu_inner">
+          <div className="logo"></div>
+          <div className="menu">
+            <ul>
+              <li>
+                <a href="#home">
+                  <img className="svg" src="img/svg/home-run.svg" alt />
+                </a>
+              </li>
+              <li>
+                <a href="#about">
+                  <img className="svg" src="img/svg/avatar.svg" alt />
+                </a>
+              </li>
+              <li>
+                <a href="#portfolio">
+                  <img className="svg" src="img/svg/briefcase.svg" alt />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="leftpart">
+        <div className="leftpart_inner">
+          <div className="logo">
+            <a href="#">
+              
+            </a>
+          </div>
+          <div className="menu">
+            <ul>
+              <li className="active">
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#portfolio">Personal Projects</a>
+              </li>
+            </ul>
+          </div>
+          <div className="copyright">
+            <p>
+              © 2023
+              <br />
+              Created by{" "}
+              <a
+                href="https://themeforest.net/item/tokyo-personal-portfolio-template/31647361"
+                target="_blank"
+              >
+                Marketify
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="rightpart">
+        <div className="rightpart_in">
+          <div id="home" className="tokyo_tm_section active">
+            <div className="container">
+              <div className="tokyo_tm_home">
+                <div className="home_content">
+                  <div className="avatar">
+                    <div className="image" data-img-url="img/slider/1.jpg" />
+                  </div>
+                  <div className="details">
+                    <h3 className="name">
+                      Prisca <span>Chidimma</span>
+                    </h3>
+                    <p className="job">
+                      Looking to solve problems and innovate through code, a
+                      dedicated junior software developer is here to join your
+                      team!
+                    </p>
+                    <div className="social">
+                      <ul>
+                        <li>
+                          <a
+                            target="_blank"
+                            href="https://github.com/Priceless-P"
+                          >
+                            <img
+                              className="svg"
+                              src="img/svg/social/facebook.svg"
+                              alt
+                            />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            target="_blank"
+                            href="http://twitter.com/priscachidimma3"
+                          >
+                            <img
+                              className="svg"
+                              src="img/svg/social/twitter.svg"
+                              alt
+                            />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            target="_blank"
+                            href="https://www.linkedin.com/in/prisca-maduka/"
+                          >
+                            <img
+                              className="svg"
+                              src="https://www.svgrepo.com/show/54425/linkedin.svg"
+                              alt
+                            />
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="about" className="tokyo_tm_section">
+            <div className="container">
+              <div className="tokyo_tm_about">
+                <div className="about_image">
+                  <img src="img/thumbs/4-2.jpg" alt />
+                  <div className="main" data-img-url="img/slider/1.jpg" />
+                </div>
+                <div className="description">
+                  <h3 className="name">
+                    Prisca Chidimma
+                  </h3>
+                  <div className="description_inner">
+                    <div className="left">
+                      <p>
+                        My name is Prisca Chidimma and I am a junior software
+                        developer. I am passionate about creating innovative and
+                        efficient software solutions that make people's lives
+                        easier. As a developer, I am skilled in a number of
+                        programming languages (Including, PHP, Python, OCaml )
+                        and I am always eager to learn new technologies to
+                        improve my skills. I have experience working on various
+                        web applications projects, and I am committed to
+                        delivering high-quality code that meets the needs of
+                        users. I am a team player and enjoy collaborating with
+                        others to solve complex problems. Thank you for
+                        considering me for any development opportunities that
+                        may arise.
+                      </p>
+                    </div>
+                    <div className="right">
+                      <ul>
+                        <li>
+                          <p>
+                            <span>Age:</span>23
+                          </p>
+                        </li>
+                        <li>
+                          <p>
+                            <span>Address:</span>Benin city, Nigeria
+                          </p>
+                        </li>
+                        <li>
+                          <p>
+                            <span>Phone:</span>
+                            <a href="tel:+2348158331726">+234 815 833 1726</a>
+                          </p>
+                        </li>
+                        <li>
+                          <p>
+                            <span>Education:</span>University of Benin
+                          </p>
+                        </li>
+                        <li>
+                          <p>
+                            <span>Email:</span>
+                            <a href="mailto:priscachidimma.maduka@gmail.com">
+                              priscachidimma.maduka@gmail.com
+                            </a>
+                          </p>
+                        </li>
+                        <li>
+                          <p>
+                            <span>Freelance:</span>Available
+                          </p>
+                        </li>
+                        <br />
+                      </ul>
+
+                      <div class="tokyo_tm_button">
+                        <a target="_blank"
+                          href="Prisca_Maduka_Resume.pdf"
+                          download="Prisca Maduka - Resume.pdf"
+                        >
+                          My Resume
+                        </a>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="portfolio" className="tokyo_tm_section">
+            <div className="container">
+              <div className="tokyo_tm_news">
+                <div className="tokyo_tm_title">
+                  <div className="title_flex">
+                    <div className="left">
+                      <h3>Projects</h3>
+                    </div>
+                  </div>
+                </div>
+                <ul>
+                  <li>
+                    <div className="list_inner">
+                      <div className="image">
+                        <img src="img/thumbs/4-3.jpg" alt />
+                        <div className="main" data-img-url="img/news/1.jpg" />
+                      </div>
+                      <div className="details">
+                        <div className="extra">
+                          <div className="short">
+                            <p className="date">
+                              Role: <span>Contributor</span>
+                            </p>
+                          </div>
+                        </div>
+                        <h3 className="title">
+                          <a href="https://eval360.app/">
+                            Eval360: A skill evaluator tool that is used for
+                            assessing engineers in an organisation.
+                          </a>
+                        </h3>
+                        <div className="tokyo_tm_read_more">
+                          <a href="https://eval360.app/">
+                            <span>Visit Website</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="list_inner">
+                      <div className="image">
+                        <img src="img/thumbs/4-3.jpg" alt />
+                        <div className="main" data-img-url="img/news/2.jpg" />
+                      </div>
+                      <div className="details">
+                        <div className="extra">
+                          <div className="short">
+                            <p className="date">
+                              Role <span>Creator</span>
+                            </p>
+                          </div>
+                        </div>
+                        <h3 className="title">
+                          <a href="https://github.com/Priceless-P/ecom">
+                            Ecommerce Website: Created with Laravel.
+                          </a>
+                        </h3>
+                        <div className="tokyo_tm_read_more">
+                          <a href="https://github.com/Priceless-P/ecom">
+                            <span>View Code</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="list_inner">
+                      <div className="image">
+                        <img src="img/thumbs/4-3.jpg" alt />
+                        <div className="main" data-img-url="img/news/3.jpg" />
+                      </div>
+                      <div className="details">
+                        <div className="extra">
+                          <div className="short">
+                            <p className="date">
+                              Role <span>Creator</span>
+                            </p>
+                          </div>
+                        </div>
+                        <h3 className="title">
+                          <a href="#">
+                            HashPython: A console app, that takes a csv file,
+                            hashes each line and returns the file with the
+                            hashes attached.
+                          </a>
+                        </h3>
+                        <div className="tokyo_tm_read_more">
+                          <a href="#">
+                            <span>View Code</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mouse-cursor cursor-outer" />
+      <div className="mouse-cursor cursor-inner" />
+    </div>
+  );
+}
+
+export default Home;
